6a954fd31ffc2bc8483e3b8dd449d89c00dc950f
Jerry Jeon <jkj8790@gmail.com>
2022-09-25 14:13:58 +0900

    Apply spanStyle to detection title

--- src/main/kotlin/ui/ExceptionDetectionView.kt
+++ src/main/kotlin/ui/ExceptionDetectionView.kt
@@ -4,24 +4,29 @@ import DetectionKey
 import DetectionResultFocus
 import MyTheme
 import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.LocalTextStyle
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.KeyboardArrowDown
 import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import detection.ExceptionDetection
 import detection.ExceptionDetectionResult
 import log.SampleData
 
@@ -34,48 +39,36 @@ fun ExceptionDetectionView(
     CompositionLocalProvider(
         LocalTextStyle provides LocalTextStyle.current.copy(fontSize = 12.sp),
     ) {
-        ExceptionDetectionRequestViewTurnedOn(
-            detectionResultFocus,
-            moveToPreviousOccurrence,
-            moveToNextOccurrence
-        )
-    }
-}
+        Box(modifier = Modifier.border(1.dp, Color.LightGray, RoundedCornerShape(4.dp))) {
+            Column(Modifier.padding(8.dp)) {
+                Text(AnnotatedString("Exception", spanStyle = ExceptionDetection().detectedStyle))
+                detectionResultFocus?.let {
+                    Column {
+                        Row {
+                            if (it.focusing == null) {
+                                Text(
+                                    "${it.results.size} results",
+                                    modifier = Modifier.align(Alignment.CenterVertically)
+                                )
+                            } else {
+                                Text(
+                                    "${it.currentIndexInView} / ${detectionResultFocus.totalCount}",
+                                    modifier = Modifier.align(Alignment.CenterVertically)
+                                )
+                            }
+                            IconButton(onClick = { moveToPreviousOccurrence(it) }) {
+                                Icon(Icons.Default.KeyboardArrowUp, "Previous Occurrence")
+                            }
+                            IconButton(onClick = { moveToNextOccurrence(it) }) {
+                                Icon(Icons.Default.KeyboardArrowDown, "Next Occurrence")
+                            }
+                        }
 
-@Composable
-private fun ExceptionDetectionRequestViewTurnedOn(
-    detectionResultFocus: DetectionResultFocus?,
-    moveToPreviousOccurrence: (DetectionResultFocus) -> Unit,
-    moveToNextOccurrence: (DetectionResultFocus) -> Unit
-) {
-    Row(Modifier.padding(8.dp)) {
-        Text("Exceptions")
-        Icon(Icons.Default.Close, "Search", Modifier.align(Alignment.CenterVertically))
-        detectionResultFocus?.let {
-            Column {
-                Row {
-                    if (it.focusing == null) {
-                        Text(
-                            "${it.results.size} results",
-                            modifier = Modifier.align(Alignment.CenterVertically)
-                        )
-                    } else {
-                        Text(
-                            "${it.currentIndexInView} / ${detectionResultFocus.totalCount}",
-                            modifier = Modifier.align(Alignment.CenterVertically)
-                        )
+                        // TODO cleanup ; don't cast
+                        (it.focusing as? ExceptionDetectionResult)?.let {
+                            Text(it.exception)
+                        }
                     }
-                    IconButton(onClick = { moveToPreviousOccurrence(it) }) {
-                        Icon(Icons.Default.KeyboardArrowUp, "Previous Occurrence")
-                    }
-                    IconButton(onClick = { moveToNextOccurrence(it) }) {
-                        Icon(Icons.Default.KeyboardArrowDown, "Next Occurrence")
-                    }
-                }
-
-                // TODO cleanup ; don't cast
-                (it.focusing as? ExceptionDetectionResult)?.let {
-                    Text(it.exception)
                 }
             }
         }
--- src/main/kotlin/ui/JsonDetectionView.kt
+++ src/main/kotlin/ui/JsonDetectionView.kt
@@ -1,23 +1,29 @@
 package ui
 
 import DetectionResultFocus
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.LocalTextStyle
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.KeyboardArrowDown
 import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import detection.JsonDetection
 
 @Composable
 fun JsonDetectionView(
@@ -28,51 +34,45 @@ fun JsonDetectionView(
     CompositionLocalProvider(
         LocalTextStyle provides LocalTextStyle.current.copy(fontSize = 12.sp),
     ) {
-        JsonDetectionRequestViewTurnedOn(
-            detectionResultFocus,
-            moveToPreviousOccurrence,
-            moveToNextOccurrence
-        )
-    }
-}
-
-@Composable
-private fun JsonDetectionRequestViewTurnedOn(
-    detectionResultFocus: DetectionResultFocus?,
-    moveToPreviousOccurrence: (DetectionResultFocus) -> Unit,
-    moveToNextOccurrence: (DetectionResultFocus) -> Unit
-) {
-    Row(Modifier.padding(8.dp)) {
-        Text("Jsons")
-        Icon(Icons.Default.Close, "Search", Modifier.align(Alignment.CenterVertically))
-        detectionResultFocus?.let {
-            Column {
-                Row {
-                    if (it.focusing == null) {
-                        Text(
-                            "${it.results.size} results",
-                            modifier = Modifier.align(Alignment.CenterVertically)
-                        )
-                    } else {
-                        Text(
-                            "${it.currentIndexInView} / ${detectionResultFocus.totalCount}",
-                            modifier = Modifier.align(Alignment.CenterVertically)
-                        )
-                    }
-                    IconButton(onClick = { moveToPreviousOccurrence(it) }) {
-                        Icon(Icons.Default.KeyboardArrowUp, "Previous Occurrence")
-                    }
-                    IconButton(onClick = { moveToNextOccurrence(it) }) {
-                        Icon(Icons.Default.KeyboardArrowDown, "Next Occurrence")
+        Box(modifier = Modifier.border(1.dp, Color.LightGray, RoundedCornerShape(4.dp))) {
+            Column(Modifier.padding(8.dp)) {
+                val title = buildAnnotatedString {
+                    append("Json ")
+                    withStyle(JsonDetection().detectedStyle) {
+                        append("{ \"key\" : \"value\" }")
                     }
                 }
+                Text(title)
+                detectionResultFocus?.let {
+                    Column {
+                        Row {
+                            if (it.focusing == null) {
+                                Text(
+                                    "${it.results.size} results",
+                                    modifier = Modifier.align(Alignment.CenterVertically)
+                                )
+                            } else {
+                                Text(
+                                    "${it.currentIndexInView} / ${detectionResultFocus.totalCount}",
+                                    modifier = Modifier.align(Alignment.CenterVertically)
+                                )
+                            }
+                            IconButton(onClick = { moveToPreviousOccurrence(it) }) {
+                                Icon(Icons.Default.KeyboardArrowUp, "Previous Occurrence")
+                            }
+                            IconButton(onClick = { moveToNextOccurrence(it) }) {
+                                Icon(Icons.Default.KeyboardArrowDown, "Next Occurrence")
+                            }
+                        }
 
-                // TODO cleanup ; don't cast
-/*
-                (it.focusing as? JsonDetectionResult)?.let {
-                    Text(it.jsonList) // TODO show summary of json
+                        // TODO cleanup ; don't cast
+                        /*
+                                        (it.focusing as? JsonDetectionResult)?.let {
+                                            Text(it.jsonList) // TODO show summary of json
+                                        }
+                        */
+                    }
                 }
-*/
             }
         }
     }
