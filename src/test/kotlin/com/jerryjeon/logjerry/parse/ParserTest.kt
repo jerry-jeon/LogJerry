f10252c62fde5330859569bd4e3cb20644d3b09c
Jerry Jeon <jkj8790@gmail.com>
2022-09-25 14:14:00 +0900

    Clean up test codes

--- src/test/kotlin/com/jerryjeon/logjerry/parse/ParserTest.kt
+++ src/test/kotlin/com/jerryjeon/logjerry/parse/ParserTest.kt
@@ -1,8 +1,8 @@
 package com.jerryjeon.logjerry.parse
 
-import Log
-import androidx.compose.ui.text.AnnotatedString
-import io.kotest.matchers.shouldBe
+import com.jerryjeon.logjerry.log.Log
+import io.kotest.matchers.collections.shouldBeEmpty
+import io.kotest.matchers.collections.shouldBeSingleton
 import org.junit.jupiter.api.Test
 
 internal class ParserTest {
@@ -13,17 +13,19 @@ internal class ParserTest {
             listOf("2021-12-19 23:05:36.664 165-165/? I/hwservicemanager: Since android.hardware.media.omx@1.0::IOmxStore/default is not registered, trying to start it as a lazy HAL.")
         )
 
-        parseResult shouldBe Log(
-            1,
-            "2021-12-19",
-            "23:05:36.664",
-            165,
-            165,
-            null,
-            "I",
-            "hwservicemanager",
-            "Since android.hardware.media.omx@1.0::IOmxStore/default is not registered, trying to start it as a lazy HAL.",
-            AnnotatedString("")
-        )
+        parseResult.logs.shouldBeSingleton {
+            Log(
+                number = 1,
+                date = "2021-12-19",
+                time = "23:05:36.664",
+                pid = 165,
+                tid = 165,
+                packageName = null,
+                priorityText = "I",
+                tag = "hwservicemanager",
+                log = "Since android.hardware.media.omx@1.0::IOmxStore/default is not registered, trying to start it as a lazy HAL.",
+            )
+        }
+        parseResult.invalidSentences.shouldBeEmpty()
     }
 }
