be95811a502735748c9a7c11d1ebb568f6dbb04e
Jerry Jeon <jkj8790@gmail.com>
2022-09-25 14:13:58 +0900

    Make find UI prettier

--- src/main/kotlin/KeywordFindView.kt
+++ src/main/kotlin/KeywordFindView.kt
@@ -1,18 +1,23 @@
+import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
+import androidx.compose.material.LocalTextStyle
+import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
-import androidx.compose.material.TextField
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.KeyboardArrowDown
 import androidx.compose.material.icons.filled.KeyboardArrowUp
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun KeywordFindView(
@@ -23,37 +28,81 @@ fun KeywordFindView(
     moveToPreviousOccurrence: (DetectionResultFocus) -> Unit,
     moveToNextOccurrence: (DetectionResultFocus) -> Unit,
 ) {
-    when (keywordFindRequest) {
-        is KeywordFindRequest.TurnedOn -> {
-            Row {
-                TextField(
-                    value = keywordFindRequest.keyword,
-                    onValueChange = {
-                        find(it)
-                    }
+    CompositionLocalProvider(
+        LocalTextStyle provides LocalTextStyle.current.copy(fontSize = 12.sp),
+    ) {
+        when (keywordFindRequest) {
+            is KeywordFindRequest.TurnedOn -> {
+                KeywordFindRequestViewTurnedOn(
+                    keywordFindRequest,
+                    find,
+                    setFindEnabled,
+                    detectionResultFocus,
+                    moveToPreviousOccurrence,
+                    moveToNextOccurrence
                 )
-                Spacer(Modifier.width(8.dp))
-                IconButton(onClick = {
-                    setFindEnabled(false)
-                }) {
-                    Icon(Icons.Default.Close, "Close find")
-                }
-
-                detectionResultFocus?.let {
-                    Text("${it.focusingResult.detectionIndexInView}/ ${detectionResultFocus.totalCount}", modifier = Modifier.align(Alignment.CenterVertically))
-                    IconButton(onClick = {
-                        moveToPreviousOccurrence(it)
-                    }) {
-                        Icon(Icons.Default.KeyboardArrowUp, "Previous Occurrence")
-                    }
-                    IconButton(onClick = {
-                        moveToNextOccurrence(it)
-                    }) {
-                        Icon(Icons.Default.KeyboardArrowDown, "Next Occurrence")
-                    }
-                }
             }
+            KeywordFindRequest.TurnedOff -> {}
         }
-        KeywordFindRequest.TurnedOff -> {}
+    }
+}
+
+@Composable
+private fun KeywordFindRequestViewTurnedOn(
+    keywordFindRequest: KeywordFindRequest.TurnedOn,
+    find: (String) -> Unit,
+    setFindEnabled: (Boolean) -> Unit,
+    detectionResultFocus: DetectionResultFocus?,
+    moveToPreviousOccurrence: (DetectionResultFocus) -> Unit,
+    moveToNextOccurrence: (DetectionResultFocus) -> Unit
+) {
+    Box(Modifier.padding(8.dp)) {
+        OutlinedTextField(
+            value = keywordFindRequest.keyword,
+            onValueChange = { find(it) },
+            leadingIcon = { Icon(Icons.Default.Search, "Search") },
+            trailingIcon = {
+                Row {
+                    detectionResultFocus?.let {
+                        Text(
+                            "${it.focusingResult.detectionIndexInView}/ ${detectionResultFocus.totalCount}",
+                            modifier = Modifier.align(Alignment.CenterVertically)
+                        )
+                        IconButton(onClick = { moveToPreviousOccurrence(it) }) {
+                            Icon(Icons.Default.KeyboardArrowUp, "Previous Occurrence")
+                        }
+                        IconButton(onClick = { moveToNextOccurrence(it) }) {
+                            Icon(Icons.Default.KeyboardArrowDown, "Next Occurrence")
+                        }
+                    }
+                    IconButton(onClick = { setFindEnabled(false) }) {
+                        Icon(Icons.Default.Close, "Close find")
+                    }
+                }
+            },
+            singleLine = true
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun KeywordFindViewPreview() {
+    MyTheme {
+        val results = listOf(
+            DetectionResult("keyword", 0, 1),
+            DetectionResult("keyword", 1, 1),
+            DetectionResult("keyword", 2, 1),
+            DetectionResult("keyword", 3, 1),
+            DetectionResult("keyword", 4, 1),
+        )
+        KeywordFindView(
+            KeywordFindRequest.TurnedOn("Searching keyword"),
+            DetectionResultFocus(results[2], results),
+            {},
+            {},
+            {},
+            {}
+        )
     }
 }
