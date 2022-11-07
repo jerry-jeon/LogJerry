d9d24a03f8bdbb41457b428e14ac3e909d8e36c6
Jerry Jeon <jkj8790@gmail.com>
2022-11-08 00:06:51 +0900

    Update version to 1.3.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,8 @@
 import org.jetbrains.compose.compose
+import org.jetbrains.compose.desktop.application.dsl.TargetFormat
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import java.io.FileInputStream
+import java.util.Properties
 
 plugins {
     kotlin("jvm") version "1.7.10"
@@ -9,7 +12,7 @@ plugins {
 }
 
 group = "com.jerryjeon"
-version = "1.2.0"
+version = "1.3.0"
 
 repositories {
     google()
@@ -36,8 +39,33 @@ tasks.test {
     useJUnitPlatform()
 }
 
+val signingProperties = Properties().apply {
+    load(FileInputStream(File(rootProject.rootDir, "signing.properties")))
+}
+
 compose.desktop {
     application {
+        javaHome = System.getenv("JDK_18")
         mainClass = "com.jerryjeon.logjerry.MainKt"
+        nativeDistributions {
+            targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
+            packageName = "LogJerry"
+
+            macOS {
+                iconFile.set(project.file("LogJerry.icns"))
+                bundleID = "com.jerryjeon.logjerry"
+                signing {
+                    sign.set(signingProperties.getProperty("compose.desktop.mac.sign").toBoolean())
+                    identity.set(signingProperties.getProperty("compose.desktop.mac.signing.identity"))
+                }
+                notarization {
+                    appleID.set(signingProperties.getProperty("compose.desktop.mac.notarization.appleID"))
+                    password.set(signingProperties.getProperty("compose.desktop.mac.notarization.password"))
+                }
+                appStore = signingProperties.getProperty("compose.desktop.mac.appStore").toBoolean()
+                // provisioningProfile.set(project.file(signingProperties.getProperty("compose.desktop.mac.provisionProfile")))
+                // runtimeProvisioningProfile.set(project.file(signingProperties.getProperty("compose.desktop.mac.runtimeProvisionProfile")))
+            }
+        }
     }
 }
