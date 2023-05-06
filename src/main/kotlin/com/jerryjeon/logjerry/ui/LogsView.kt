9f5d39efcd101a1f267b75419071c360e4486292
Jerry Jeon <jerry.jeon@sendbird.com>
2023-05-06 16:15:27 +0900

    Enhance visibility of marked row

--- src/main/kotlin/com/jerryjeon/logjerry/ui/LogsView.kt
+++ src/main/kotlin/com/jerryjeon/logjerry/ui/LogsView.kt
@@ -1,19 +1,21 @@
 package com.jerryjeon.logjerry.ui
 
 import androidx.compose.foundation.VerticalScrollbar
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.RowScope
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.rememberScrollbarAdapter
 import androidx.compose.material.Divider
+import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import com.jerryjeon.logjerry.ColumnDivider
 import com.jerryjeon.logjerry.HeaderDivider
+import com.jerryjeon.logjerry.detector.DetectorKey
 import com.jerryjeon.logjerry.detector.JsonDetection
 import com.jerryjeon.logjerry.log.Log
 import com.jerryjeon.logjerry.logview.LogSelection
@@ -42,17 +44,29 @@ fun LogsView(
             logs.forEach { refinedLog ->
                 item {
                     Column {
-                        LogRow(
-                            refinedLog = refinedLog,
-                            preferences = preferences,
-                            header = header,
-                            selected = refinedLog == logSelection?.refinedLog,
-                            divider = divider,
-                            collapseJsonDetection = collapseJsonDetection,
-                            expandJsonDetection = expandJsonDetection,
-                            toggleMark = toggleMark,
-                            selectLog = selectLog
-                        )
+                        val logRow: @Composable () -> Unit = {
+                            LogRow(
+                                refinedLog = refinedLog,
+                                preferences = preferences,
+                                header = header,
+                                selected = refinedLog == logSelection?.refinedLog,
+                                divider = divider,
+                                collapseJsonDetection = collapseJsonDetection,
+                                expandJsonDetection = expandJsonDetection,
+                                toggleMark = toggleMark,
+                                selectLog = selectLog
+                            )
+                        }
+                        val marked = DetectorKey.Mark in refinedLog.detectionFinishedLog.detections.keys
+                        if(marked) {
+                            Box(modifier = Modifier.fillMaxWidth().wrapContentHeight().background(Color.Cyan).padding(start = 4.dp, top = 30.dp, end = 4.dp, bottom = 4.dp)) {
+                                Box(modifier = Modifier.background(MaterialTheme.colors.background)) {
+                                    logRow()
+                                }
+                            }
+                        } else {
+                            logRow()
+                        }
                         Divider()
                     }
                 }
